9b9740f1ba653af5c313f9681159275f44ac56bd
TheKiruls <amirin856@gmail.com>
2024-06-13 22:19:28 +0800

    live server using node.js

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+module.exports = [
+    {
+      files: ["**/*.js"],
+      rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double"]
+      }
+    }
+  ];
